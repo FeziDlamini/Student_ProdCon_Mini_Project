a94ca068c774bab4b6d49648d7967ecb0979a130
FeziDlamini <fezidlamini140@gmail.com>
2023-06-27 15:24:02 +0200

    ITStudent class creation

---
+++ it_student.js
@@ -0,0 +1,9 @@
+class ITStudent {
+    constructor (sname, sid, programme, courselist){
+        this.sname=sname;
+        this.sid=sid;
+        this.programme=programme;
+        this.courselist=courselist; 
+    }
+    
+}
