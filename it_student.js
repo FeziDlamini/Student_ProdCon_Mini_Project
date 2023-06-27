4ac4a801cbac3232ebaffd3d3c6693ad352b2713
saheedfaremi <saheed.faremi@curnance.com>
2023-06-27 15:28:43 +0200

    add: functions for IT student

--- it_student.js
+++ it_student.js
@@ -1,9 +1,18 @@
 class ITStudent {
-    constructor (sname, sid, programme, courselist){
-        this.sname=sname;
-        this.sid=sid;
-        this.programme=programme;
-        this.courselist=courselist; 
+    constructor(sname, sid, programme, courselist) {
+        this.sname = sname;
+        this.sid = sid;
+        this.programme = programme;
+        this.courselist = courselist;
     }
-    
+
+    calculateAverage() {
+        const totalMarks = this.courses.reduce((sum, course) => sum + course.mark, 0);
+        return totalMarks / this.courses.length;
+    }
+
+    determinePassOrFail() {
+        const average = this.calculateAverage();
+        return average >= 50 ? 'Pass' : 'Fail';
+    }
 }
