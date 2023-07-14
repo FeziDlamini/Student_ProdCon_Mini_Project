d7d52d0df4577e2811760d2f250ef40a028e3100
saheedfaremi <saheed.faremi@curnance.com>
2023-07-14 08:14:37 +0200

    implement semaphore for buffer control

---
+++ semaphor.js
@@ -0,0 +1,25 @@
+class Semaphore {
+    constructor(initialCount) {
+        this.count = initialCount;
+        this.waitQueue = [];
+    }
+
+    acquire() {
+        if (this.count > 0) {
+            this.count--;
+        } else {
+            return new Promise((resolve) => {
+                this.waitQueue.push(resolve);
+            });
+        }
+    }
+
+    release() {
+        if (this.waitQueue.length > 0) {
+            const nextProcess = this.waitQueue.shift();
+            nextProcess();
+        } else {
+            this.count++;
+        }
+    }
+}
